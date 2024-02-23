fe3f62a85a0403744434011b085d6d1dda149bb8
Ananyab3100 <ananyabhagat2000@gmail.com>
2024-02-23 13:34:56 +0530

    commit third

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ export default class App extends Component {
         <Nav />
         <Routes>
           <Route exact path='/'element={<News key='general' pageSize ={9} country='in' category='general'/>}/>
-          <Route exact path='/business' element={<News key='business' pageSize ={9} country='in' category='business'/>}/>
-          <Route exact path='/entertainment' element={<News key='entertainment' pageSize ={9} country='in' category='entertainment'/>}/>
-          <Route exact path='/general' element={<News key='general' pageSize ={9} country='in' category='general'/>}/>
-          <Route exact path='/health' element={<News key='health' pageSize ={9} country='in' category='health'/>}/>
-          <Route exact path='/science' element={<News key='science' pageSize ={9} country='in' category='science'/>}/>
-          <Route exact path='/sports' element={<News key='sports' pageSize ={9} country='in' category='sports'/>}/>
-          <Route exact path='/technology' element={<News key='echnology' pageSize ={9} country='in' category='technology'/>}/>
+          <Route  path='/business' element={<News key='business' pageSize ={9} country='in' category='business'/>}/>
+          <Route  path='/entertainment' element={<News key='entertainment' pageSize ={9} country='in' category='entertainment'/>}/>
+          <Route  path='/general' element={<News key='general' pageSize ={9} country='in' category='general'/>}/>
+          <Route  path='/health' element={<News key='health' pageSize ={9} country='in' category='health'/>}/>
+          <Route  path='/science' element={<News key='science' pageSize ={9} country='in' category='science'/>}/>
+          <Route  path='/sports' element={<News key='sports' pageSize ={9} country='in' category='sports'/>}/>
+          <Route  path='/technology' element={<News key='echnology' pageSize ={9} country='in' category='technology'/>}/>
         </Routes>
         
       </div>
